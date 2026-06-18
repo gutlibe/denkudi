5f6ea84284447276f140007d4eeda667d8d7aab0
gutlibe <gutlibe24@gmail.com>
2026-06-18 17:11:34 +0000

    feat(ui): update branding to HTU E-Voting and clean up favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,7 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
@@ -39,7 +38,7 @@
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'HTU E-Voting') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
