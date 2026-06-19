f0d40f1d23a62e9fd17d12eacb9e6fd742d8f10a
gutlibe <gutlibe24@gmail.com>
2026-06-19 08:33:09 +0000

    chore(enums): add label and options methods to Role enum

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -6,4 +6,20 @@ enum Role: string
 {
     case Student = 'student';
     case Admin = 'admin';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Student => 'Student',
+            self::Admin => 'Admin',
+        };
+    }
+
+    public static function options(): array
+    {
+        return array_reduce(self::cases(), fn ($carry, $role) => [
+            ...$carry,
+            $role->value => $role->label(),
+        ], []);
+    }
 }
