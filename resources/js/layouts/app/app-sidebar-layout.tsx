3eeaf1ec9f3b2e36895f31d9b5df95e906b5ff1f
gutlibe <gutlibe24@gmail.com>
2026-06-20 08:34:12 +0000

    feat: restructure app layout with liquid background and mobile support

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -1,20 +1,23 @@
 import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
-import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { LiquidBackground } from '@/components/liquid-background';
+import { MobileFloatingHeader } from '@/components/mobile-floating-header';
 import type { AppLayoutProps } from '@/types';
 
 export default function AppSidebarLayout({
     children,
-    breadcrumbs = [],
 }: AppLayoutProps) {
     return (
         <AppShell variant="sidebar">
             <AppSidebar />
             <AppContent variant="sidebar" className="max-lg:max-h-svh max-lg:overflow-y-auto [&::-webkit-scrollbar]:hidden [scrollbar-width:none]">
-                <AppSidebarHeader breadcrumbs={breadcrumbs} className="bg-transparent" />
-                <div className="flex flex-1 flex-col gap-4 px-4 py-6 lg:px-6 lg:py-8">
-                    {children}
+                <LiquidBackground />
+                <div className="relative z-10 flex flex-1 flex-col">
+                    <MobileFloatingHeader />
+                    <div className="flex flex-1 flex-col gap-4 overflow-x-hidden px-4 pt-16 pb-6 md:pt-4 lg:px-6 lg:py-8">
+                        {children}
+                    </div>
                 </div>
             </AppContent>
         </AppShell>
