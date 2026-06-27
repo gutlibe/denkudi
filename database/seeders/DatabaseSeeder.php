f07aad78793201930036fc2a86e5d76d31f06553
gutlibe <gutlibe24@gmail.com>
2026-06-27 10:23:52 +0000

    chore: make DatabaseSeeder safe by default, demo seeder runs on demand

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Enums\ElectionStatus;
-use App\Enums\ElectionType;
-use App\Models\Candidate;
-use App\Models\Election;
-use App\Models\Position;
-use App\Models\User;
-use App\Models\Vote;
-use App\Services\VotingService;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(DemoSeeder::class);
+        //
     }
 }
